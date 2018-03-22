b02e57ba1989e293a2fbd111f109cce800acd98d
Roop Pal <roop.pal@columbia.edu>
2018-03-22 17:49:40 -0400

    initialized catkin workspace

---
+++ folding_ws/devel/setup.sh
@@ -0,0 +1,1 @@
+/home/roop/Code/robotic-laundry-folding/folding_ws/devel/.private/catkin_tools_prebuild/setup.sh
---
+++ folding_ws/devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/roop/Code/robotic-laundry-folding/folding_ws/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
